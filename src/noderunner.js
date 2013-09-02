918f2c8d9897f125047988204e572038e0583a63
Håvard Holvik <havard@local-host.no>
2013-09-02 10:04:03 +0200

    Removed rmdir timeout.

--- src/noderunner.js
+++ src/noderunner.js
@@ -42,11 +42,7 @@ module.exports = {
 			currentProcess = spawn(startCommand,startArgs,{cwd: dirName});
 			currentProcess.path = dirName;
 			
-			if (oldDir){
-				setTimeout(function(){
-					rmdir(oldDir, function(err){ });
-				},10000);
-			}
+			if (oldDir) rmdir(oldDir, function(err){ });
 		}
 		
 		// Set initial busy state		
