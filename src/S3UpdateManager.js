7c531a5dd82a52660c3cf3f88603cda8ca016276
Håvard Holvik <havard@local-host.no>
2013-09-04 10:05:43 +0200

    Etc

--- src/S3UpdateManager.js
+++ src/S3UpdateManager.js
@@ -27,6 +27,7 @@ S3UpdateManager.prototype.getLatestVersion = function(cb) {
 		self.busy = true;
 		
 		// Poll file info
+		console.log(self.s3Path);
 		self.s3Client.headFile(self.s3Path, function(err, res){		
 			if (err) 
 			{
