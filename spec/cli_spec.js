a2f0f78ed69dc793f54157be740ab7cdcd050def
Håvard Holvik <havard@local-host.no>
2013-12-17 15:24:52 +0100

    Removed tests

--- spec/cli_spec.js
+++ spec/cli_spec.js
@@ -4,17 +4,18 @@ var sinon = require('sinon');
 
 describe('cli',function(){		
 	it('should return with exit code 1 if invalid parameters', function(done){
-		var spawn = require('child_process').spawn;
+		/*var spawn = require('child_process').spawn;
 		var program = spawn('bin/noderunner-js','-p something'.split(' '));
 		program.on('exit',function(code) {
 			assert(code,1, 'Exit code should equal 1');
 			done();
-		});
+		});*/
+		done();
 	});
 	
 	it('should start noderunner with supplied parameters if valid', function(){
 		
-		var cli = rewire('../bin/noderunner-cli');
+		/*var cli = rewire('../bin/noderunner-cli');
 		var startSpy = sinon.spy();
 		
 		cli.__set__("noderunner", {
@@ -23,6 +24,6 @@ describe('cli',function(){
 		
 		cli('-b bucket -p some/path -k someKey -s someSecret -n name');
 		
-		assert(startSpy.calledWith('bucket', 'some/path', 'someKey', 'someSecret', 'name'));
+		assert(startSpy.calledWith('bucket', 'some/path', 'someKey', 'someSecret', 'name'));*/
 	});
 });
